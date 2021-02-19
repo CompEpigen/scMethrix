0cfb0bc15336888faf22234a92910fb57f2437d1
knacko <geekbike@hotmail.com>
2021-02-19 10:22:48 +0100

    Added cast for ranges (needed for subset function)

--- R/accessory_funcs.R
+++ R/accessory_funcs.R
@@ -30,3 +30,35 @@ non_vect_code = function(files,verbose = FALSE,col_idx) {
   
   }
 }
+
+#--------------------------------------------------------------------------------------------------------------------------
+# Parse genomic regions and convert them to key'd data.table
+cast_ranges <- function(regions, set.key = TRUE) {
+  chr <- . <- NULL
+  if (is(regions, "GRanges")) {
+    target_regions <- data.table::as.data.table(x = regions)
+    target_regions[, `:=`(seqnames, as.character(seqnames))]
+    colnames(target_regions)[seq_len(3)] <- c("chr", "start", "end")
+    if (set.key){
+      data.table::setDT(x = target_regions, key = c("chr", "start", "end"))}
+    target_regions <- target_regions[, .(chr, start, end)]
+  } else if (is(regions, "data.frame")) {
+    if (all(c("chr", "start", "end") %in% colnames(regions))){
+      regions <- regions[,c("chr", "start", "end")]
+    } else {
+      warning("Columns with names chr, start and end are not found. Assuming that the first three columns are chr, start and end.")
+    }
+    target_regions <- data.table::as.data.table(x = regions)
+    colnames(target_regions)[seq_len(3)] <- c("chr", "start", "end")
+    target_regions <- target_regions[, .(chr, start, end)]
+    target_regions[, `:=`(chr, as.character(chr))]
+    target_regions[, `:=`(start, as.numeric(as.character(start)))]
+    target_regions[, `:=`(end, as.numeric(as.character(end)))]
+    if (set.key){
+      data.table::setDT(x = target_regions, key = c("chr", "start", "end"))}
+  } else {
+    stop("Invalid input class for regions. Must be a data.table or GRanges object")
+  }
+  
+  target_regions
+}
