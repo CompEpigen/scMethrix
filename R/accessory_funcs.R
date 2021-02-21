edb9b0bed1fbb41bd006145a57902723f0d66fe7
knacko <geekbike@hotmail.com>
2021-02-21 23:49:52 +0100

    Added function to capture scores from tabix file

--- R/accessory_funcs.R
+++ R/accessory_funcs.R
@@ -3,39 +3,6 @@ is_ondisk = function(m) {
 }
 
 
-non_vect_code = function(files,verbose = FALSE,col_idx) {
-  
-  for (i in 1:length(files)) {
-  
-    if (i == 1) {
-      
-      data <- if(exists(data)) cbind(data,data_temp) else data_temp
-      
-    }
-    
-    
-    # optimize the input window
-    y <- optimal rows
-    x <- optimal cols
-    
-    for (i in 1:(ncols(data)/y)) {  
-      
-      data_temp <- fread(input = files[i],
-                       select = (x*(i-1)+1):min(x*i,cols), # min() catches % remainder
-                       header = TRUE,
-                       sep = "\t",
-                       stringsAsFactors = FALSE,
-                       #colClasses = rep("numeric", 970),
-                       col.names = col_idx,
-                       data.table = TRUE)
-    
-      data <- data_temp %>% as.matrix() %>% dropNA() %>% cbind(data, .)
-    
-    }
-  
-  }
-}
-
 #--------------------------------------------------------------------------------------------------------------------------
 # Parse genomic regions and convert them to key'd data.table
 cast_ranges <- function(regions, set.key = TRUE) {
@@ -67,3 +34,30 @@ cast_ranges <- function(regions, set.key = TRUE) {
   
   target_regions
 }
+
+cast_granges <- function(regions) {
+  
+  
+  
+  
+}
+
+#------------------------------------------------------------------------
+# Gets the scores from a given tabix file for a specific region (including NA)
+get_tabix_scores <- function(file, regions){
+  
+  if(length(tbx) = 0) stop("Input tabix file has no entries.")
+
+  tbx = Rsamtools::scanTabix(file, param = regions)
+  tbx[lengths(tbx) == 0] <- NA_character_
+  tbx <- lapply(tbx,function(x) {
+                  if (is.na(x)) return(NA)
+                  else return (unlist(strsplit(x,"\t"))[4])
+  })
+
+  tbx2 <- do.call(rbind.data.frame, tbx)
+
+  colnames(tbx2) <- strsplit(basename(file), "[.]")[[1]][1]
+  
+}
+
