a5b3cd4b4f81ad7bd788c1ebd5cc1c4320e42c50
knacko <geekbike@hotmail.com>
2021-02-22 16:37:32 +0100

    Renamed bedgraph generator and increased functionality

---
+++ R/generate_bedgraphs.R
@@ -0,0 +1,27 @@
+setwd("D:\\Documents\\School\\Thesis\\scMethrix\\test.data\\10gen")
+library(data.table)
+
+### Convert to tabix in bash
+# From bash:
+# find . -name "*.bedgraph" -exec bgzip {} \;
+# find . -name "*.gz" -exec tabix -p bed {} \;
+
+### Generate sequential bedgraphs
+numfiles <- 10
+numrows <- 100 #per chromosome
+chrs <- 10 # must be factor of numrows
+rangeFactor <- 3 # max value is rangeFactor*numrows
+randomize = FALSE # set to true if genomic regions should be random
+  
+for (n in 1:numfiles) {
+  
+  range <- sample(x = 1:(numrows*chrs*rangeFactor), size = chrs*numrows)
+  if (!randomize) range <- sort(range)
+  names <- rep(c(1:chrs),each=numrows)
+  
+  data.test <- data.frame(chr = paste("chr",names,sep=""), start = range, end = range+1, val = sample(x = 0:4)*25)
+  fwrite(data.test, file=paste("bed",n,".bedgraph",sep=""), quote=FALSE, sep='\t', col.names = FALSE, scipen=999)
+  
+  print(n)
+  
+}
