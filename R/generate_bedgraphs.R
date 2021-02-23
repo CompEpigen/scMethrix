a096ec531a8af648be0a5d61f85fde5ab9efaf80
knacko <geekbike@hotmail.com>
2021-02-23 11:57:43 +0100

    Fixed inconsistent values in bedgraph generator

--- R/generate_bedgraphs.R
+++ R/generate_bedgraphs.R
@@ -18,8 +18,9 @@ for (n in 1:numfiles) {
   range <- sample(x = 1:(numrows*chrs*rangeFactor), size = chrs*numrows)
   if (!randomize) range <- sort(range)
   names <- rep(c(1:chrs),each=numrows)
+  values <- sample(x = 0:4*25, size = chrs*numrows, replace = TRUE)
   
-  data.test <- data.frame(chr = paste("chr",names,sep=""), start = range, end = range+1, val = sample(x = 0:4)*25)
+  data.test <- data.frame(chr = paste("chr",names,sep=""), start = range, end = range+1, val = values)
   fwrite(data.test, file=paste("bed",n,".bedgraph",sep=""), quote=FALSE, sep='\t', col.names = FALSE, scipen=999)
   
   print(n)
