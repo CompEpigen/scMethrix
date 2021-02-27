a4cd5e02f31946304beb861cdaf580866bcb2b4d
knacko <geekbike@hotmail.com>
2021-02-27 22:58:31 +0100

    Added tests and started test data

---
+++ tests/testobj.R
@@ -0,0 +1,14 @@
+contigs <- c("chr1","chr3")
+regions <- GRanges(seqnames = "chr1", ranges = IRanges(1,10000)) 
+samples <- c("bed1","bed3")
+
+chr <- rep(c("chr1","chr2","chr3"),each=1000)
+start <- 1:3000
+end <- 1:3000+1
+value <- rep(c(0,25,50,75,100),600)
+
+bed1 <- data.frame(chr,start,end,value)
+bed2 <- data.frame(chr,start+1500,end+1500,value)
+names(bed2) <- c("chr","start","end","value")
+bed3 <- data.frame(chr,start+3000,end+3000,value)
+names(bed3) <- c("chr","start","end","value")
--- tests/testthat/test-accessory_funcs.R
+++ tests/testthat/test-accessory_funcs.R
@@ -1,3 +1,29 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+data('scMethrix_data')
+mem <- NULL
+tbx <- NULL
+
+test_that("is_ondisk",{
+  expect_true(is_ondisk(tbx))
+  expect_false(is_ondisk(mem))
 })
+
+test_that("get_sample_name", {
+  expect_equal("filename",get_sample_name("c:/dir/dir.dir/filename.extension.extension"))
+})
+
+test_that("get_files",{
+  
+})
+
+test_that("chunk_granges",{
+  expect_true(all(rowRanges(mem) == unlist(chunk_granges(rowRanges(mem),factor=factor,percent=percent,num=num))))
+  expect_true(all(rowRanges(tbx) == unlist(chunk_granges(rowRanges(tbx),factor=factor,percent=percent,num=num))))
+})
+
+test_that("cast_granges",{})
+
+test_that("order_by_sd",{})
+
+test_that("subset_scMethrix",{})
+
+test_that("get_stats",{})
