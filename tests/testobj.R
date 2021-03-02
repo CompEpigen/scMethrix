b263e9d2f25e0d8921df12401fbc961d04b20445
knacko <geekbike@hotmail.com>
2021-03-02 23:22:25 +0100

    Updated test object

--- tests/testobj.R
+++ tests/testobj.R
@@ -1,3 +1,5 @@
+library(methylKit)
+
 contigs <- c("chr1","chr3")
 regions <- GRanges(seqnames = "chr1", ranges = IRanges(1,10000)) 
 samples <- c("bed1","bed3")
@@ -12,3 +14,9 @@ bed2 <- data.frame(chr,start+1500,end+1500,value)
 names(bed2) <- c("chr","start","end","value")
 bed3 <- data.frame(chr,start+3000,end+3000,value)
 names(bed3) <- c("chr","start","end","value")
+
+methylKit::df2tabix(bed1,"bed1_test.bed")
+methylKit::df2tabix(bed2,"bed2.bed")
+methylKit::df2tabix(bed3,"bed3.bed")
+
+
