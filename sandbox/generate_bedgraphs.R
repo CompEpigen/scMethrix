c4fed918bd86bf5dbe62afebce4bab39f7076d50
knacko <geekbike@hotmail.com>
2021-03-11 17:39:56 +0100

    Changed sparsity to a min and max sparsity

--- sandbox/generate_bedgraphs.R
+++ sandbox/generate_bedgraphs.R
@@ -1,15 +1,17 @@
-setwd("D:\\Documents\\School\\Thesis\\scMethrix\\sample.data\\")
+library(data.table)
+setwd("D:\\Documents\\School\\Thesis\\scMethrix\\sample.data\\bedtools")
 
 ### Convert to tabix in bash
 # From bash:
 # find . -name "*.bedgraph" -exec bgzip {} \; 
 # find . -name "*.gz" -exec tabix -p bed {} \;
 
-numfiles = 100      # Number of files to generate
-numrows = 1000000  # Max number of CpG sites in sample
+numfiles = 5      # Number of files to generate
+numrows = 1000     # Max number of CpG sites in sample
 chrs = 10          # Number of chromosomes
-sparsity = 0.5     # Minimum sparsity (minrows = numrows*sparsity)
-rangeFactor = 50   # Max range of IRange (1:rangeFactor*numrows)
+minsparsity = 0.5     # Minimum sparsity (minrows = numrows*sparsity)
+maxsparsity = 0.8 # Max sparsity
+rangeFactor = 1   # Max range of IRange (1:rangeFactor*numrows)
 randomize = FALSE  # Randomize the chr and IRange mapping
 values <- c(0,25,50,75,100) # List of values to choose from
 
@@ -21,10 +23,11 @@ start.time <- Sys.time()
     if (!randomize) range <- sort(range)
     names <- rep(c(1:chrs),each=(numrows/chrs))
     val <- sample(values, size = numrows, replace = TRUE)
-    dat <- data.frame(chr = paste("chr",names,sep=""), start = range, end = range+1, val = val)
-    dat <- dat[sample(NROW(dat), NROW(dat)*(runif(1, sparsity, 1))),]
+    dat <- data.frame(chr = names, start = range, end = range+1, val = val)
+    dat <- dat[sample(NROW(dat), NROW(dat)*(runif(1, minsparsity, maxsparsity ))),]
     dat <- dat[with(dat, order(chr, start, end)),]
-    fwrite(dat, file = paste0("bed",n,".bedgraph"), quote=FALSE, sep='\t', col.names = FALSE, scipen=999)
+    dat$chr <- paste("chr",dat$chr,sep="")
+    fwrite(dat, file = paste0("bed",formatC(n, width=3, flag="0"),".bedgraph"), quote=FALSE, sep='\t', col.names = FALSE, scipen=999)
     
     message(paste("Writing:",n))
 
@@ -32,4 +35,7 @@ start.time <- Sys.time()
 
 message(paste0("Generating ",n," files took ",round(Sys.time() - start.time,2),"s"))
         
+
+
+
         
