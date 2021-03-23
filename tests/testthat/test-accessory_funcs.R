327eae25c3e36c7c3ce32ff927af4e8e9c5d9919
knacko <geekbike@hotmail.com>
2021-03-23 11:00:56 +0100

    Updated tests in accessory functions

--- tests/testthat/test-accessory_funcs.R
+++ tests/testthat/test-accessory_funcs.R
@@ -1,23 +1,32 @@
-data('scMethrix_data')
-mem <- NULL
-tbx <- NULL
+
+df1 <- data.table(chr=rep("chr1",5),start=1:5,end=2:6,value=0)
+df2 <- data.table(chr=rep("chr1",5),start=3:7,end=4:8,value=0)
+df3 <- data.table(chr=rep("chr1",5),start=6:10,end=7:11,value=0)
+
+files <- c("df1.bedgraph","df2.bedgraph","df3.bedgraph")
+files <- file.path(tempdir(),files)
+
+write.table(df1, file = files[1], row.names=FALSE, sep="\t",col.names=FALSE, quote = FALSE)
+write.table(df2, file = files[2], row.names=FALSE, sep="\t",col.names=FALSE, quote = FALSE)
+write.table(df3, file = files[3], row.names=FALSE, sep="\t",col.names=FALSE, quote = FALSE)
+
+scm.h5 <- read_beds(files,h5=TRUE)
+scm.mem <- read_beds(files,h5=FALSE)
 
 test_that("is_ondisk",{
-  expect_true(is_ondisk(tbx))
-  expect_false(is_ondisk(mem))
+  expect_true(is_h5(scm.h5))
+  expect_false(is_h5(scm.mem))
 })
 
 test_that("get_sample_name", {
-  expect_equal("filename",get_sample_name("c:/dir/dir.dir/filename.extension.extension"))
-})
-
-test_that("get_files",{
-  
+  expect_equal("file.name",get_sample_name("c:/dir/dir.dir/file.name.extension"))
 })
 
 test_that("chunk_granges",{
-  expect_true(all(rowRanges(mem) == unlist(chunk_granges(rowRanges(mem),factor=factor,percent=percent,num=num))))
-  expect_true(all(rowRanges(tbx) == unlist(chunk_granges(rowRanges(tbx),factor=factor,percent=percent,num=num))))
+  
+  expect_error(chunk_granges("not scMethrix"))
+  
+  expect_true(all(rowRanges(scm.h5) == unlist(chunk_granges(rowRanges(scm.h5),factor=factor,percent=percent,num=num))))
 })
 
 test_that("cast_granges",{})
