a00da3f1efeca5ffc2c7433a88eb82ad8e71b537
knacko <geekbike@hotmail.com>
2021-04-13 17:20:47 +0200

    Added dataset documentation

---
+++ R/scMethrix_data.R
@@ -0,0 +1,16 @@
+#' Simulated WGBS data
+#'
+#' This is a temporary small set of data representing CpGs on chr1 and chr2 in regions 1:20
+#'
+#' @docType data
+#'
+#' @usage data('scMethrix_data')
+#'
+#' @format A list containing two objects of class \code{'scMethrix'}, one in mem and one in H5 format.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data('scMethrix_data')
+#' scMethrix_data
+"scMethrix_data"
