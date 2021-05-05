b6e4d6bb8dd84ce9f72bd5f59f5359c5308021fd
knacko <geekbike@hotmail.com>
2021-05-05 11:42:33 -0400

    Revert "Updated test data"
    This reverts commit 564c47417693953345ca8f464d372ef07a405280.

--- R/scMethrix_data.R
+++ R/scMethrix_data.R
@@ -1,6 +1,6 @@
-#' WGBS for single cell methylome of murine neuronal cells 
+#' Simulated WGBS data
 #'
-#' This is a subset of the original converted to `scMethrix` format
+#' This is a temporary small set of data representing CpGs on chr1 and chr2 in regions 1:20
 #'
 #' @docType data
 #'
@@ -9,9 +9,7 @@
 #' @format A list containing two objects of class \code{'scMethrix'}, one in mem and one in H5 format.
 #'
 #' @keywords datasets
-#' 
-#' @references Luo, C. et al. (2017). Single-cell methylomes identify neuronal subtypes and regulatory 
-#' 
+#'
 #' @examples
 #' data('scMethrix_data')
 #' scMethrix_data
