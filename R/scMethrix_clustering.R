8925de5a4b28ab7af29350296ca020b297231677
knacko <geekbike@hotmail.com>
2021-08-23 15:06:11 -0600

    Added basic cluster function

--- R/scMethrix_clustering.R
+++ R/scMethrix_clustering.R
@@ -30,8 +30,8 @@
 #' @param verbose boolean; flag to output messages or not
 #' @return matrix; the distance matrix
 #' @import bioDist
-#' @seealso https://www.rdocumentation.org/packages/stats/versions/3.6.2/topics/dist
-#' @seealso https://www.bioconductor.org/packages//2.7/bioc/html/bioDist.html
+#' @seealso <https://www.rdocumentation.org/packages/stats/versions/3.6.2/topics/dist>
+#' @seealso <https://www.bioconductor.org/packages//2.7/bioc/html/bioDist.html>
 #' @examples
 #' @export
 get_distance_matrix <- function(scm, assay="score",type="spearman",verbose=TRUE) {
@@ -51,5 +51,33 @@ get_distance_matrix <- function(scm, assay="score",type="spearman",verbose=TRUE)
     return(tau.dist(mtx))
   } 
   
+}
+
+#' Generates a cluster object for an \code{\link{scMethrix}} object
+#' @details Enables multiple methods of clustering to classify samples in an \code{\link{scMethrix}} object. Either an \code{\link{scMethrix}} object or a \code{\link[stats]{dist}} object must be provided for clustering.
+#' @param scm scMethrix; Input \code{\link{scMethrix}} object. If this is specified the distance matrix will be a generic \code{\link[bioDist]{spearman.dist}} distance
+#' @param dist dist; A distance matrix generated for an scMethrix object. Not necessary
+#' @param assay string; The assay to use. Default is 'score'
+#' @param type string; The type of distance metric to use. Available options are 'spearman' and 'tau'. 
+#' @param verbose boolean; flag to output messages or not
+#' @return matrix; the distance matrix
+#' @import bioDist
+#' @seealso [get_distance_matrix()]
+#' @examples
+#' @export
+cluster_scMethrix <- function(scm = NULL, dist = NULL, assay="score", verbose = TRUE, type="hierarchical") {
+
+  if (is.null(dist)) {
+    if (is.null(scm)) {
+      stop("Either scm or dist must be specified")
+    } else {
+      dist <- get_distance_matrix(scm, assay=assay)
+    }
+  }
+  
+  if (type=="hierarchical") {
+    fit <- hclust(dist, method="ward.D")
+    return(fit)
+  } 
 }
 
