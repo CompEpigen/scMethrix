bcce87196f0ab0cfc6ebac82b2d2b5eba2a91961
knacko <geekbike@hotmail.com>
2021-09-13 15:18:24 -0600

    Added basic collapse sample function

--- R/scMethrix_transforms.R
+++ R/scMethrix_transforms.R
@@ -70,12 +70,6 @@ transform_assay <- function(scm, assay = "score", new_assay = NULL, trans = NULL
 #' @param bin_by character; can create bins by # of base pairs "bp" or by # of CpG sites "cpg". Default "bp"
 #' @param trans named vector of closures; The transforms for each assay in a named vector. Default NULL, meaning that 
 #' operations for "counts" assay is sum(x, na.rm=TRUE), and for all other assays is mean(x, na.rm=TRUE)
-#' @param overlap_type character; defines the type of the overlap of the CpG sites with the target region. 
-#' Default value is `within`. For detailed description, see the \code{findOverlaps} function of the 
-#' \code{\link{IRanges}} package.
-#' @param batch_size integer; the number of samples to process at once
-#' @param h5_dir directory to store an H5 based object
-#' @param replace boolean; flag for whether to delete the contents of h5_dir before saving 
 #' @return An \code{\link{scMethrix}} object
 #' @examples
 #' data('scMethrix_data')
@@ -501,6 +495,108 @@ bin_scMethrix <- function(scm = NULL, regions = NULL, bin_size = 100000, bin_by 
 #   return (m_obj)
 # }
 # 
+
+#' Collapses multiple samples into a single sample
+#' @details Uses the inputted function to transform an assay in the \code{\link{scMethrix}} object. Typically, most assays will use either mean (for measurements) or sum (for counts). The transform is applied column-wise to optimize how HDF5 files access sample data. If HDF5 objects are used, transform functions should be  from \pkg{DelayedMatrixStats}.
+#' 
+#' In the output object, the number of CpGs in each region is saved in mcol(scm)$n_cpgs.
+#' @inheritParams generic_scMethrix_function
+#' @param colname string; The colname from \code{colData(scm)} indicating which samples should be collapse together
+#' @param trans named vector of closures; The transforms for each assay in a named vector. Default NULL, meaning that 
+#' operations for "counts" assay is sum(x, na.rm=TRUE), and for all other assays is mean(x, na.rm=TRUE)
+#' \code{\link{IRanges}} package.
+#' @return An \code{\link{scMethrix}} object
+#' @examples
+#' data('scMethrix_data')
+#' regions <- GRanges(seqnames = c("chr1"), ranges = IRanges(1,200000000)) 
+#' regions <- unlist(tile(regions,10))
+#' bin_scMethrix(scMethrix_data, regions = regions)
+#' @export
+collapse_samples <- function(scm = NULL, colname = NULL, trans = NULL, h5_dir = NULL, batch_size = 20, batch_size = 20, n_threads = 1, replace = FALSE, verbose = TRUE) {
+  
+  if (!is(scm, "scMethrix")) {
+    stop("A valid scMethrix object needs to be supplied.", call. = FALSE)
+  }
+  
+  if (!(colname %in% colnames(colData(scm)))) {
+    stop("Column '",colname,"' is not present in colData")
+  }
+  
+  if (is.null(h5_dir) && is_h5(scm)) stop("Output directory must be specified")
+  
+  if (is.null(trans)) {
+    trans <- c(counts = function(x) sum(x,na.rm=TRUE))
+  } else if (is.null(trans[["counts"]])) {
+    trans <- c(trans, c(counts = function(x) sum(x,na.rm=TRUE)))
+  }
+  
+  members <- data.frame(Sample = row.names(colData(scm)), Group = factor(scm@colData[,colname]))
+
+  for (name in SummarizedExperiment::assayNames(scm)) {
+    
+    if (verbose) message("   Collapsing samples for the ",name," assay...")
+    
+    if (is.null(trans[[name]])) { # If no named vector is specified, default to mean
+      op <- function(x) mean(x,na.rm=TRUE)
+    } else {
+      op <- trans[[name]]
+    }
+    
+    if (is_h5(scm)) {
+      
+      
+      
+      
+    } else {
+      
+      mtx <- data.table(get_matrix(scm,assay=name)) #TODO: Somehow missing rows if not subset, not sure why
+      mtx <- setDT(lapply(split.default(mtx, group$Group), rowSums))[]
+
+      assays[[name]] <- as(mtx,class(get_matrix(scm,assay=name)))
+      
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
