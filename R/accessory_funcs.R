81163284cd0266a55f8b5ddf36ba17b644bbaa4d
knacko <geekbike@hotmail.com>
2022-01-01 16:25:17 -0700

    Moved or deleted old junk code

--- R/accessory_funcs.R
+++ R/accessory_funcs.R
@@ -433,4 +433,37 @@ parse_source_idx = function(chr_idx = NULL, start_idx = NULL, end_idx = NULL, st
 }
 
 
-
+#--- .generate_random_bedgraph -----------------------------------------------------------------------------
+#' Creates random bedgraph files. Used for testing.
+#' @param numfile integer; Number of files to generate
+#' @param numrows integer; Max number of CpG sites in sample
+#' @param chrs integer; Number of chromosomes
+#' @param minsparsity numeric; Minimum sparsity (minrows = numrows*sparsity)
+#' @param maxsparsity numeric; Max sparsity
+#' @param rangeFactor numeric; Max range of IRange (1:rangeFactor*numrows)
+#' @param randomize boolean; Randomize the chr and IRange mapping
+#' @param values list of numerics; List of values to choose from
+#' @param dir string; the directory path to store the bedgraph files in
+#' @return NULL, with bedgraph files placed in the specified \code{dir}
+.generate_random_bedgraph <- function(numfile = 1, numrows = 1000000, chrs = 10, minsparsity = 0.5, maxsparsity = 1, rangeFactor = 2, randomize = FALSE, values = c(0,25,50,75,100), dir = NULL) {
+  
+  start.time <- Sys.time()
+  
+  for (n in 1:numfiles) {
+    
+    range <- sample(x = 1:(numrows*rangeFactor), size = numrows)
+    if (!randomize) range <- sort(range)
+    names <- rep(c(1:chrs),each=(numrows/chrs))
+    val <- sample(values, size = numrows, replace = TRUE)
+    dat <- data.frame(chr = names, start = range, end = range+1, val = val)
+    dat <- dat[sample(NROW(dat), NROW(dat)*(runif(1, minsparsity, maxsparsity ))),]
+    dat <- dat[with(dat, order(chr, start, end)),]
+    dat$chr <- paste("chr",dat$chr,sep="")
+    fwrite(dat, file = paste0(dir,"bed",formatC(n, width=3, flag="0"),".bedgraph"), quote=FALSE, sep='\t', col.names = FALSE, scipen=999)
+    message(paste("Writing:",n))
+  }
+  
+  message(paste0("Generating ",n," files took ",round(Sys.time() - start.time,2),"s"))
+  
+  return(NULL)
+}
--- sandbox/annotation.R
+++
@@ -1,8 +0,0 @@
-library("GenomicRanges")
-library("AnnotationHub")
-
-# Get Grange of promoters
-ah = AnnotationHub()
-qhs = query(ah, c("RefSeq", "Mus musculus", "mm10"))
-genes = qhs[[1]]
-proms = promoters(genes)
