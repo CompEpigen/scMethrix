3e9199d5a49b373bd49a2b3d6298b1d858b20d43
knacko <geekbike@hotmail.com>
2022-03-28 17:40:56 -0600

    Refactored sample data generator

--- data_gen/data_gen.R
+++ data_gen/data_gen.R
@@ -7,30 +7,36 @@ dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
 src_files <- list.files (dir,full.names = TRUE)
 src_files <- src_files[grepl(".*bedgraph$", src_files,ignore.case = TRUE)]
 
-read <- read_beds(files=src_files,h5=FALSE,cov_idx=c(5,6))
-m <- subset_scMethrix(read,contigs=c("chr1","chr2"))
-m <- mask_scMethrix(m, low_count = NULL, high_quantile=0.9)
+library(BSgenome.Mmusculus.UCSC.mm10)
+ref_cpgs <- suppressWarnings(scMethrix::extract_CpGs(genome = "BSgenome.Mmusculus.UCSC.mm10"))
 
-cpg <- lapply(1:length(src_files)-1, function(n) {
+#read <- read_beds(files=src_files,h5=FALSE,cov_idx=c(5,6))
+reads <- read_beds(files=src_files, chr_idx=1, start_idx=2, end_idx=3, beta_idx=4, M_idx = 5, U_idx=6, ref_cpgs = ref_cpgs)
+reads <- subset_scMethrix(reads,contigs=paste0("chr",1:5))
+#m <- mask_scMethrix(m, low_count = NULL, high_quantile=0.9)
 
-  c <- which(rowSums(is.na(get_matrix(m)))==n)
+cpg <- lapply(0:length(src_files), function(n) {
+  c <- which(rowSums(is.na(counts(reads)))==n)
   c <- sample(c,sample(50:100, 1))
-  
 })
  
-m <- m[sort(unlist(cpg))]
+reads <- reads[sort(unlist(cpg))]
 suffix <- "_sub"
 
-export_bed(m, path = dir, suffix=suffix)
+exportBed(reads, path = dir, assays = c("score","counts"), suffix=suffix, na.rm = FALSE)
 
 files <- list.files (dir,full.names = TRUE)
-files <- files[grepl(paste0(".*",suffix,".bedgraph$"), files,ignore.case = TRUE)]
+files <- files[grepl(paste0(".*.bed$"), files,ignore.case = TRUE)]
 
-file.rename(files, paste0(dir,"/C", 1:length(files),".bedgraph"))
+file.rename(files, paste0(dir,"/C", 1:length(files),".bed"))
 
 files <- list.files (dir,full.names = TRUE)
-files <- files[grepl(".*bedgraph$", files,ignore.case = TRUE)]
+files <- files[grepl(".*bed$", files,ignore.case = TRUE)]
 files <- setdiff(files, src_files)
 
 colData <- data.frame(phenotype = c(rep("C",2),rep("N",2)))
-m <- read_beds(files=files,h5=FALSE,cov_idx=5,colData = colData)
+row.names(colData) <- paste0("C",1:length(files))
+
+scm <- read_beds(files=files, chr_idx=1, start_idx=2, end_idx=3, beta_idx=4, cov_idx=5, colData = colData)
+
+#m <- read_beds(files=files,h5=FALSE,cov_idx=5,colData = colData)
